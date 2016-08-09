d1f672b56b71ce0e0c9fa632b2a73ee133ebc03d
weilgnew <weilgnew@hotmail.com>
2016-08-09 11:16:08 +0800

    Update beat_gen.py

--- beat_gen.py
+++ beat_gen.py
@@ -5,10 +5,10 @@ def beat_gen_sim(length):
     beat = []
     for i in range(0,length):
         if i%2 == 0:
-            beat.append(8)
             beat.append(4)
-            beat.append(8)
+            beat.append(2)
             beat.append(4)
+            beat.append(2)
         else:
             beat.append(4)
             beat.append(4)
@@ -16,3 +16,46 @@ def beat_gen_sim(length):
     return beat
 
 ###############################################################################
+
+def beat_gen_reg_1(length):
+    beat = []
+    beat.append(8)
+    beat.append(8)
+    beat.append(8)
+    beat.append(8)
+    for i in range(0,length):
+            beat.append(4)
+            beat.append(4)
+            beat.append(8)
+            
+            beat.append(4)
+            beat.append(4)
+            beat.append(4)
+            beat.append(4)
+            
+    return beat
+    
+###############################################################################
+
+def beat_gen_chord_2(length):
+    beat = []
+
+    for i in range(0,length):
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            beat.append(6)
+            
+    return beat
